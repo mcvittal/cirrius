e90d417a4718a16b2caccff0e61f87294816b7ea
Alex McVittie <alexander.mcvittie@gmail.com>
2019-02-23 18:36:20 -0500

    Addition of client python file

---
+++ client_fetch.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import urllib.request, os
+from sys import platform
+import subprocess as commands
+
+if platform == "linux" or platform == "linux2":
+
+    urllib.request.urlretrieve("http://alexmcvittie.me/htv3/desktop.png", "/tmp/htv.png")
+    os.system("gsettings set org.gnome.desktop.background picture-uri file:///tmp/htv.png")
+else if platform == "windows" or platform == "nt":
+    import ctypes
+    urllib.request.urlretrieve("http://alexmcvittie.me/htv3/desktop.png", "htv.png")
+    SPI_SETDESKWALLPAPER = 20 
+    ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, "htv.jpg" , 0)
+
+else:
+    print("Not supported on your platform")
