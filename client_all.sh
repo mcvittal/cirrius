685e54128883a1df1af2c273210b5c3b10b3324e
Alex McVittie <amcvittiie@skywatch.co>
2019-02-24 23:29:15 -0500

    Update

--- client_all.sh
+++ client_all.sh
@@ -1,6 +1,6 @@
 while true; do
 
-ssh ajmcvitt@taurine.csclub.uwaterloo.ca "/usr/bin/chromium --headless --disable-gpu --screenshot=/users/ajmcvitt/www/htv3/desktop.png http://alexmcvittie.me/htv3git/map.html --window-size=1366,768"
+ssh ajmcvitt@taurine.csclub.uwaterloo.ca "/usr/bin/chromium --headless --disable-gpu --screenshot=/users/ajmcvitt/www/htv3/desktop.png http://alexmcvittie.me/htv3git/map.html --window-size=1920,1080"
 ./client_fetch.py
 sleep 60
 done
